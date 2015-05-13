f7580bbd9c57a035381df6bda5fdc560ee281811
fyles94 <im@et.esiea.fr>
2015-05-13 09:22:19 +0200

    Create Affichage.java

---
+++ eternity/src/main/java/fr/esiea/glpoo/Affichage.java
@@ -0,0 +1,16 @@
+package Window;
+
+
+
+
+public class Affichage
+{
+
+
+	public static void main(String[] args)
+	{
+
+		@SuppressWarnings("unused")
+		GlobalConsoleFrame test = new GlobalConsoleFrame();
+	}
+} 
